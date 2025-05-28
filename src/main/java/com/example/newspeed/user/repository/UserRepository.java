a0b0e56e7ad07a5cc892cd6e3b0629b99e276ca1
syuare-dev <tachunsa13@gmail.com>
2025-05-28 17:02:30 +0900

    Feat: 유저 조회(단건) API 구현 - 레포지토리 > 유저 조회 시 예외 처리
    UserRepository:
    - 요청받은 userId로 유저 조회
    - 유저가 조회되지 않을 경우 404 NOT FOUND 오류 반환

--- src/main/java/com/example/newspeed/user/repository/UserRepository.java
+++ src/main/java/com/example/newspeed/user/repository/UserRepository.java
@@ -2,6 +2,18 @@ package com.example.newspeed.user.repository;
 
 import com.example.newspeed.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * 요청받은 id에 해당하는 유저 조회 > 해당 유저데이터가 없을 경우 예외 처리
+     * @param userId 조회할 유저 ID
+     * @return ID 값으로 조회한 유저 데이터
+     * @throws ResponseStatusException 유저 데이터가 없을 경우 > 404 상태 코드로 예외 처리
+     */
+    default User findByIdOrElseThrow(Long userId) {
+        return findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + userId));
+    }
 }
