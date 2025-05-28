5662c70a8f21824a13f8c399b8f6fa9552ac4a4d
Zyooon <wcw7373@gmail.com>
2025-05-28 15:03:49 +0900

    feat : follow 기능 추가
    - 파라미터로 팔로우 대상 id 넘겨줌
    - 대상이 팔로우 상태라면 언팔로우 (DB 삭제)
    - 대상이 팔로우 상태가 아니라면 팔로우(DB 추가)

---
+++ src/main/java/com/example/newspeed/user/controller/FollowController.java
@@ -0,0 +1,30 @@
+package com.example.newspeed.user.controller;
+
+import com.example.newspeed.global.common.JwtTokenProvider;
+import com.example.newspeed.user.dto.followDto;
+import com.example.newspeed.user.service.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/follow")
+@RequiredArgsConstructor
+public class FollowController {
+    private final FollowService followService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> follow(@PathVariable Long id){
+
+        Long userId = jwtTokenProvider.getUserIdFromSecurity();
+
+        followService.follow(new followDto(userId, id));
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/newspeed/user/service/FollowService.java
@@ -0,0 +1,40 @@
+package com.example.newspeed.user.service;
+
+import com.example.newspeed.user.dto.followDto;
+import com.example.newspeed.user.entity.Follow;
+import com.example.newspeed.user.entity.User;
+import com.example.newspeed.user.repository.FollowRepository;
+import com.example.newspeed.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+
+    private final UserRepository userRepository;
+    private final FollowRepository followRepository;
+
+    public void follow(followDto followDto) {
+        Long userId = followDto.getUserId();
+        Long followId = followDto.getFollowId();
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("요청자 유저 없음"));
+
+        User follow = userRepository.findById(followId)
+                .orElseThrow(() -> new IllegalArgumentException("팔로우 대상 유저 없음"));
+
+        Optional<Follow> existingFollow = followRepository.findByUserIdAndFollowId(userId, followId);
+
+        if (existingFollow.isPresent()) {
+            // 팔로우 상태면 언팔로우
+            followRepository.delete(existingFollow.get());
+        } else {
+            // 팔로우 안되어 있으면 팔로우 추가
+            followRepository.save(new Follow(user, follow));
+        }
+    }
+}
