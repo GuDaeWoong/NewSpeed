7142057ec56092ac61a6335417177f31ecf1ad9b
syuare-dev <tachunsa13@gmail.com>
2025-05-28 16:46:12 +0900

    Feat: 유저 조회(단건) API 구현 : 응답 DTO 생성
    - User 엔티티 정보의 응답 DTO 생성
    - toDto():
     > User 엔티티를 FindUserResponseDto 로 변환
     > - 1) 유저가 작성한 Post 리스트(getPost())를 가져오기
     > - 2) 각 Post를 (Post::toDto) 와 같이 DTO로 변환하기
     > - 3) Post 리스트를 DTO 리스트로 변환하기
    
     - followCount > 주석 처리
     > 향후 follow 가 추가될 경우 follow 수 계산하는 코드 미리 작성 후 주석 처리

---
+++ src/main/java/com/example/newspeed/user/dto/FindUserResponseDto.java
@@ -0,0 +1,60 @@
+package com.example.newspeed.user.dto;
+
+import com.example.newspeed.post.dto.PostTitleOfUserDto;
+import com.example.newspeed.post.entity.Post;
+import com.example.newspeed.user.entity.User;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class FindUserResponseDto {
+
+    private final Long userId;
+
+    private final String email;
+
+    private final String nickname;
+
+    private String userUrl;
+
+    // 팔로우 수 -> 일단 주석 처리
+//    private final Long followCount;
+
+    private final LocalDateTime createdAt;
+
+    private final LocalDateTime modifiedAt;
+
+    private final List<PostTitleOfUserDto> posts;
+
+    public FindUserResponseDto(
+            Long userId, String email, String nickname, String userUrl,
+//            Long followCount,
+            LocalDateTime createdAt, LocalDateTime modifiedAt, List<PostTitleOfUserDto> posts)
+    {
+        this.userId = userId;
+        this.email = email;
+        this.nickname = nickname;
+        this.userUrl = userUrl;
+//        this.followCount = followCount;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.posts = posts;
+    }
+
+    public static FindUserResponseDto toDto (User user) {
+        return new FindUserResponseDto(
+                user.getId(),
+                user.getEmail(),
+                user.getNickname(),
+                user.getUserUrl(),
+//                user.getFollow.size(), // 팔로우 수 (User 가 팔로우한 유저 목록의 크기)
+                user.getCreatedAt(),
+                user.getModifiedAt(),
+                user.getPosts().stream() // 유저의 Post 리스트
+                        .map(Post::toDto) // 각 Post 를 DTO 로 변환
+                        .toList() // Post 리스트를 DTO 리스트로 변환
+        );
+    }
+}
