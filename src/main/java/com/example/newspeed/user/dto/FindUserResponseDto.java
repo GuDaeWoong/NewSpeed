bc6cea9815c35637bfab8f204b350506a807e9ac
syuare-dev <tachunsa13@gmail.com>
2025-05-29 14:02:13 +0900

    Refactor: 유저 조회 > Post 내용 삭제
    FindUserResponseDto
    - Post 리스트를 가져오는 내용 삭제
    - 삭제 이유: User 와 Post 의 역할을 명확히 구분하기 위함

--- src/main/java/com/example/newspeed/user/dto/FindUserResponseDto.java
+++ src/main/java/com/example/newspeed/user/dto/FindUserResponseDto.java
@@ -26,12 +26,10 @@ public class FindUserResponseDto {
 
     private final LocalDateTime modifiedAt;
 
-    private final List<PostTitleOfUserDto> posts;
-
     public FindUserResponseDto(
             Long userId, String email, String nickname, String userUrl,
             Long followCount,
-            LocalDateTime createdAt, LocalDateTime modifiedAt, List<PostTitleOfUserDto> posts)
+            LocalDateTime createdAt, LocalDateTime modifiedAt)
     {
         this.userId = userId;
         this.email = email;
@@ -40,7 +38,6 @@ public class FindUserResponseDto {
         this.followCount = followCount;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
-        this.posts = posts;
     }
 
     public static FindUserResponseDto toDto (User user, long followCount) {
@@ -51,10 +48,7 @@ public class FindUserResponseDto {
                 user.getUserUrl(),
                 followCount, // 팔로우 수 (User 가 팔로우한 유저 목록의 크기)
                 user.getCreatedAt(),
-                user.getModifiedAt(),
-                user.getPosts().stream() // 유저의 Post 리스트
-                        .map(Post::toDto) // 각 Post 를 DTO 로 변환
-                        .toList() // Post 리스트를 DTO 리스트로 변환
+                user.getModifiedAt()
         );
     }
 }
