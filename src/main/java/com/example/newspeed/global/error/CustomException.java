2c7d9f52ed74c24a4291cb21885f44c2ecb50d95
Zyooon <wcw7373@gmail.com>
2025-05-29 17:57:17 +0900

    feat : CustomException 추가
    - 에러메세지 직접 넣어서 보낼 경우 사용하도록 추가

---
+++ src/main/java/com/example/newspeed/global/error/CustomException.java
@@ -0,0 +1,18 @@
+package com.example.newspeed.global.error;
+
+import com.example.newspeed.global.Enums.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+    //에러코드와 에러메세지 커스텀으로 만들어서 예외처리
+
+    private final HttpStatus status;
+
+    //에러 메세지 양식 직접 날려준다.
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.status = errorCode.getStatus();
+    }
+}
